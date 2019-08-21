af3713a12b98523e31ff67dce20fb15fa65f4ba8
Paul Chabinga <paulwchabz23@gmail.com>
2019-08-21 10:35:14 +0000

    Automatically backed up by Learn

---
+++ arrays.js
@@ -0,0 +1,15 @@
+var chocolateBars = ["snickers","hundred grand","kitkat","skittles"]
+var chocolateBarz = ["snickers","hundred grand","kitkat","skittles"]
+
+function addElementToBeginningOfArray(chocolates, element) {
+  return chocolates.unshift(element);
+}
+
+
+function destructivelyAddElementToBeginningOfArray(chocolates, element){
+  return [element, ...chocolates]
+  
+}
+
+addElementToBeginningOfArray(chocolateBarz, "Galaxy")
+destructivelyAddElementToBeginningOfArray(chocolateBars, "Galaxy")
